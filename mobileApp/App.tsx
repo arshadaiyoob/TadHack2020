480c413651ad2139bedea4309ad4dd47662e9b86
Arshad Aiyoob <arshadaiyoob19@gmail.com>
2020-10-09 01:06:27 +0530

    first commit

--- mobileApp/App.tsx
+++ mobileApp/App.tsx
@@ -2,13 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+interface Props {}
+interface State {}
+
+export default class App extends React.Component<Props, State>{
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your application.</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({

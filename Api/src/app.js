cd16be5b5c370648b777b872c8a926bb944b2bcb
Arshad Aiyoob <arshadaiyoob19@gmail.com>
2020-10-10 18:36:40 +0530

    otp

--- Api/src/app.js
+++ Api/src/app.js
@@ -10,11 +10,11 @@ let sms = new api();
 app.use(bodyParser.json());
 const middleware = "/api/v2/"
 app.use(middleware+"user/", userRoute);
-<<<<<<< HEAD
+
 sms.runServer();
-=======
+
 app.use(middleware+"covid/", covidRoute);
->>>>>>> 5824b2890ff7d7c074e64738aa1675cddf40870a
+
 app.listen(4000, () =>{
     console.log("Server is running on port 4000");
 })

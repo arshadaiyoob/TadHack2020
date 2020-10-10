191e7b6309b35b6f25d05fb1193a6ed3225c7499
Izzath97 <izzath0@gmail.com>
2020-10-10 21:58:34 +0530

    updated covid routes

--- Api/src/routes/user.route.js
+++ Api/src/routes/user.route.js
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
-const router = require("express").Router();
-const { User } = require("../db/models");
 
-router.post("/validateOTP", async (req, res) => {
-	let body = req.body;
-});
-
-router.get("/generateOTP", async (req, res) => {
-	let body = req.body;
-	let OTP = Math.floor(100000 + Math.random() * 900000);
-	res.status(200).send({ OTP });
-});
-
-router.post("/register", async (req, res) => {
-	let body = req.body;
-	//Checking User already exist
-	const userExist = await User.findOne({ phone: body.phone });
-	if (userExist) return res.status(400).send("User already Exist!");
-
-	let newUser = new User(body);
-
-	newUser.save().then(() => {
-		return res.status(200).send(newUser);
-	});
-});
-
-router.post("/login", async (req, res) => {
-	let phone = req.body.phone;
-	let password = req.body.password;
-	User.findByCredentials(phone, password).then((user) => {
-		return res.status(200).send(user.toJson());
-	});
-});
-
-module.exports = router;
-=======
 const router = require('express').Router();
 const { User } = require('../db/models');
 const api = require('../api/sms');
@@ -80,4 +44,3 @@ router.post('/login', async(req, res) =>{
 })
 
 module.exports = router;
->>>>>>> db3394c36b6f0af149cdf7dcbb384c6093ebb15a

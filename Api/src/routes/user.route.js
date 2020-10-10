377b4715e0ba29a64538c9efcaad736ae0894a8e
Arshad Aiyoob <arshadaiyoob19@gmail.com>
2020-10-10 18:33:15 +0530

    OTP

--- Api/src/routes/user.route.js
+++ Api/src/routes/user.route.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { User } = require('../db/models');
+const api = require('../api/sms');
 
+
+const sms = new api();
 router.post('/register', async (req, res) =>{
     let body = req.body;
     //Checking User already exist
@@ -11,9 +14,21 @@ router.post('/register', async (req, res) =>{
     
     newUser.save()
     .then(() =>{
+        sms.sendOTP(body.phone)
         return res.status(200).send(newUser);
     })
 })
+
+router.post('/OTP', async(req,res) => {
+    let otp = req.body.otp;
+    let verify = await User.verifyOTP(otp);
+    if(verify == req.body.phone){
+        res.status(200).send({status: "success"})
+    }
+    else if(verify == false){
+        res.status(401).send("OTP Invalid!")
+    }
+});
 
 router.post('/login', async(req, res) =>{
     let phone = req.body.phone;
